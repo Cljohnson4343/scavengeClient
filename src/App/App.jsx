2dd62ab6dadfcc14e34c13d1af4b2691e1ebcf18
Chris Johnson <cljohnson4343@gmail.com>
2019-04-06 17:02:29 -0500

    fix AppBar fixed position problem.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import AppBar from '../AppBar';
 import HomePage from '../HomePage';
+import { withStyles } from '@material-ui/core';
 
+const styles = {
+  appBarOffset: {
+    marginTop: '50px',
+  },
+}
 class App extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
       <div>
         <AppBar />
-        <HomePage />
+        <div className={classes.appBarOffset} >
+          <HomePage />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
