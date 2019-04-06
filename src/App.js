2a0d9dbd8a4b03224d1efc87e51f4be4763bd125
Chris Johnson <cljohnson4343@gmail.com>
2019-04-06 10:37:26 -0500

    added Material-UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core';
+
+const styles = {
+  root: {
+    backgroundColor: 'red',
+  },
+};
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className={this.props.classes.root}>
+      My first hunt!!!
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
