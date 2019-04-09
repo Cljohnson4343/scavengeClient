f8fdc339afb823f2507e51a42c27782c83bea6d4
Chris Johnson <cljohnson4343@gmail.com>
2019-04-09 18:22:55 -0500

    added HuntInfoAddFilterButton.

---
+++ src/HuntInfoContainer/HuntInfoAddFilterButton/HuntInfoAddFilterButton.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Menu, MenuItem, } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import FilterButton from '../../utils/FilterButton';
+import { filtersObj } from '../HuntInfoFilters/filters';
+
+
+function HuntInfoAddFilterButton(props) {
+    const { addFilter, ...other } = props;
+
+    const [ anchorEl, setAnchorEl ] = useState(null);
+
+    const handleFilterMenuClose = e => setAnchorEl(null);
+
+    let filterMenuItems = [];
+    for (let key in filtersObj) {
+        filterMenuItems.push(
+            <MenuItem
+                onClick={e => {
+                    addFilter(filtersObj[key]);
+                    handleFilterMenuClose(e);
+                }}
+                key={filtersObj[key].displayString}
+            >
+                {filtersObj[key].displayString}
+            </MenuItem>
+        );
+
+    };
+
+    return (
+        <div>
+            <FilterButton 
+                aria-owns='filter-menu'
+                aria-haspopup='true'
+                onClick={e => setAnchorEl(e.currentTarget)}
+                {...other}
+            >
+                <AddIcon fontSize='small'/>
+            </FilterButton>
+            <Menu
+                anchorEl={anchorEl}
+                id='filter-menu'
+                open={Boolean(anchorEl)}
+                onClose={handleFilterMenuClose}
+            >
+                {filterMenuItems}
+            </Menu>
+        </div>
+    );
+};
+
+HuntInfoAddFilterButton.propTypes = {
+    addFilter: PropTypes.func.isRequired,
+
+};
+
+export default HuntInfoAddFilterButton;
