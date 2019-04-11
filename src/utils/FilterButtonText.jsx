adfdc552059f49ea92d50c50dd7053dad3f38ddb
Chris Johnson <cljohnson4343@gmail.com>
2019-04-11 10:03:24 -0500

    added a clear icon to FilterButtonText to allow user to associate the filter text on screen with a button.

--- src/utils/FilterButtonText.jsx
+++ src/utils/FilterButtonText.jsx
@@ -2,28 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, withTheme } from '@material-ui/core';
 import classNames from 'classnames';
+import ClearIcon from '@material-ui/icons/Clear';
 
 const styles = theme => ({
     font: {
-        fontSize: '0.4em',
+        fontSize: '0.6em',
         paddingRight: theme.spacing.unit/2,
         paddingLeft: theme.spacing.unit/2,
     },
     // Empty object to allow consumers to style text
     root: {
+        flex: '0 0 auto',
+        alignItems: 'center',
     },
+    container: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+    },
+    icon: {
+        fontSize: '0.7em',
+        paddingRight: theme.spacing.unit/2,
+    }
 });
 
 function FilterButtonText(props) {
     const { classes, ...other } = props;
 
     return (
-        <span 
-            className={classNames(classes.font, classes.root)}
-            {...other}
-        >
-            {props.children}
-        </span>
+        <div className={classes.container}>
+            <span 
+                className={classNames(classes.font, classes.root)}
+                {...other}
+            >
+                {props.children}
+            </span>
+            <ClearIcon color='primary' classes={{ root: classes.font }} />
+        </div>
     );
 };
 
